bd9fd210211e637350eb8427b6ebc6496a7e5e17
AlGouvea <alvarohsgouvea@gmail.com>
2024-06-25 18:47:44 -0300

    Refatoração de Serializers para conexão do endpoint

--- backend/chatbotApi/chats/serializers.py
+++ backend/chatbotApi/chats/serializers.py
@@ -1,37 +1,30 @@
 from rest_framework import serializers
 from .models import Chat, Message
-from transformers import pipeline
 from django.contrib.auth.models import User
+from .endpoint import bot_response_api
 
 
 class MessageSerializer(serializers.ModelSerializer):
-
-    text_generator = pipeline("text-generation", model="gpt2")
-
     class Meta:
         model = Message
         fields = ["id", "chat", "user", "content"]
 
-    def get_bot_response(self, message):
-        generated_text = self.text_generator(
-            message, max_length=500, num_return_sequences=1
-        )
-        return generated_text[0]["generated_text"]
-
     def create(self, request):
-        new_message = super().create(request)
+        if request.get('user').id != 1:
+            new_message = super().create(request)
+            return new_message
+        else:
+            user_message = request.get("content")
+            bot_message = bot_response_api(user_message)
 
-        user_message = request.get("content")
-        bot_message = self.get_bot_response(user_message)
+            bot_response = {
+                "chat": request.get("chat"),
+                "user": User.objects.get(id=1),
+                "content": bot_message,
+            }
+            response = super().create(bot_response)
+            return response
 
-        bot_response = {
-            "chat": request.get("chat"),
-            "user": User.objects.get(id=1),
-            "content": bot_message,
-        }
-        super().create(bot_response)
-
-        return new_message
 
 
 class ChatSerializer(serializers.ModelSerializer):
